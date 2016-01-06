5a723ce62930c7b67c1b4d48c19b0e4c1989a7f9
Behram ÇELEN <behramcelen@gmail.com>
2016-01-06 11:07:51 +0200

    create bundle file structure

---
+++ Controller/DefaultController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EP\DisplayBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+}
---
+++ Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EP\DisplayBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+}
