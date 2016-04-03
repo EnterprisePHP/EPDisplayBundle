fe37f78b1a922ae571c1b90dd73cc6f9bebff16c
Behram ÇELEN <behramcelen@gmail.com>
2016-04-04 02:05:46 +0300

    load bundle configs as parameter from ep display extension

--- DependencyInjection/EPDisplayExtension.php
+++ DependencyInjection/EPDisplayExtension.php
@@ -4,6 +4,7 @@ namespace EP\DisplayBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -22,7 +23,62 @@ class EPDisplayExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $this->setupBundleConfigs($container, $config);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    /**
+     * @param ContainerInterface $container
+     * @param $configs
+     */
+    private function setupBundleConfigs(ContainerInterface $container, $configs)
+    {
+        $globalConfig = $configs['global'];
+
+        //set image_render config as param
+        if(isset($globalConfig['image_render'])){
+            $container->setParameter('ep_display.config.image_render', $globalConfig['image_render']);
+        }else{
+            $container->setParameter('ep_display.config.image_render', true);
+        }
+
+        //set file_render config as param
+        if(isset($globalConfig['file_render'])){
+            $container->setParameter('ep_display.config.file_render', $globalConfig['file_render']);
+        }else{
+            $container->setParameter('ep_display.config.file_render', true);
+        }
+
+        //set templating config as param
+        if(isset($globalConfig['templating'])){
+            $container->setParameter('ep_display.config.templating', $globalConfig['templating']);
+        }else{
+            $container->setParameter('ep_display.config.templating', 'EPDisplayBundle:display.html.twig');
+        }
+
+        //set exclude_vars config as param
+        if(isset($globalConfig['exclude_vars'])){
+            $container->setParameter('ep_display.config.exclude_vars', $globalConfig['exclude_vars']);
+        }else{
+            $container->setParameter('ep_display.config.exclude_vars', []);
+        }
+
+        //set array_collection_render config as param
+        if(isset($globalConfig['array_collection_render'])){
+            $container->setParameter('ep_display.config.array_collection_render', $globalConfig['array_collection_render']);
+        }else{
+            $container->setParameter('ep_display.config.array_collection_render', true);
+        }
+
+        //set collection_item_count config as param
+        if(isset($globalConfig['collection_item_count'])){
+            $container->setParameter('ep_display.config.collection_item_count', $globalConfig['collection_item_count']);
+        }else{
+            $container->setParameter('ep_display.config.collection_item_count', 10);
+        }
+
+        return;
     }
 }
