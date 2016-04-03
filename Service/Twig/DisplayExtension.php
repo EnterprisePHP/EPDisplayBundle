b5a6c6cdba0cd86e9c94266e19ce45c521c62850
Behram ÇELEN <behramcelen@gmail.com>
2016-04-04 02:45:36 +0300

    inject bundle configs to display extension and create exclude and expose vars adders

--- Service/Twig/DisplayExtension.php
+++ Service/Twig/DisplayExtension.php
@@ -2,6 +2,10 @@
 
 namespace EP\DisplayBundle\Service\Twig;
 
+use EP\DisplayBundle\Annotation\Exclude;
+use EP\DisplayBundle\Annotation\Expose;
+use EP\DisplayBundle\Annotation\File;
+use EP\DisplayBundle\Annotation\Image;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Translation\TranslatorInterface;
 use Doctrine\Common\Annotations\Reader;
@@ -52,6 +56,16 @@ class DisplayExtension extends \Twig_Extension
     private $entity;
 
     /**
+     * @var array
+     */
+    private $bundleConfigs = [];
+
+    /**
+     * @var array
+     */
+    private $configs = [];
+
+    /**
      * @var
      */
     private $normalizedEntity;
@@ -59,12 +73,26 @@ class DisplayExtension extends \Twig_Extension
     public function __construct(
         TranslatorInterface $translator,
         Reader $reader,
-        Twig_Environment $twig
+        Twig_Environment $twig,
+        $imageRender,
+        $fileRender,
+        $template,
+        $excludeVars,
+        $arrayCollectionRender,
+        $collectionItemCount
     )
     {
-        $this->translator = $translator;
-        $this->reader = $reader;
-        $this->twig = $twig;
+        $this->translator       = $translator;
+        $this->reader           = $reader;
+        $this->twig             = $twig;
+        $this->bundleConfigs    = [
+            'image_render'              => $imageRender,
+            'file_render'               => $fileRender,
+            'template'                  => $template,
+            'exclude_vars'              => $excludeVars,
+            'array_collection_render'   => $arrayCollectionRender,
+            'collection_item_count'     => $collectionItemCount,
+        ];
     }
 
     public function getFunctions()
@@ -84,11 +112,48 @@ class DisplayExtension extends \Twig_Extension
         if(!method_exists($entity, 'display')){
             throw new Exception('Please create an public display method for object');
         }
+        $this->entity = $entity;
+        $this->setupBundleConfigs();
+        $this->setupAnnotationOptions();
         $this->normalizeConfigurations();
+    }
+
+    private function setupBundleConfigs($options = [])
+    {
+        $this->configs['image_render'] = $this->bundleConfigs['image_render'];
+        $this->configs['file_render'] = $this->bundleConfigs['file_render'];
+        $this->configs['template'] = $this->bundleConfigs['template'];
+        $this->configs['exclude_vars'] = $this->bundleConfigs['exclude_vars'];
+        $this->configs['array_collection_render'] = $this->bundleConfigs['array_collection_render'];
+        $this->configs['collection_item_count'] = $this->bundleConfigs['collection_item_count'];
     }
 
     private function normalizeConfigurations()
     {
+    }
+
+    /**
+     * @param $var
+     */
+    private function addExcludeVar($var)
+    {
+        if(!in_array($var, $this->excludeVars)){
+            $this->excludeVars[] = $var;
+            return;
+        }
+        return;
+    }
+
+    /**
+     * @param $var
+     */
+    private function addExposeVar($var)
+    {
+        if(!in_array($var, $this->exposeVars)){
+            $this->exposeVars[] = $var;
+            return;
+        }
+        return;
     }
 
     public function getName()
